831223f4ce9d782a7de326a918b603d9c19e281e
rieerafaelx <rafaeledoyaga@gmail.com>
2024-04-16 21:55:33 +0800

    Update muna

---
+++ lib/Style/appstyle.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NoteStyle {
+  static Color bgColor = const Color (0xFFe2e2ff);
+  
+  
+  static List<Color> cardsColor = [
+
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+
+  ];
+
+}
+
